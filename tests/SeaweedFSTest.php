1aa779034358072e9679d7da12ecf2414aef1359
hbh112233abc <hbh112233abc@163.com>
2024-03-11 15:10:35 +0800

    add SeaweedFS test case

---
+++ tests/SeaweedFSTest.php
@@ -0,0 +1,78 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use bingher\obs\driver\S3;
+use Noodlehaus\Config;
+
+class SeaweedFSTest extends TestCase
+{
+    protected $s3;
+
+    protected function setUp(): void
+    {
+        $conf     = Config::load(__DIR__ . '/../.env.ini');
+        $this->s3 = new S3($conf['SeaweedFS']);
+    }
+
+    public function testPut()
+    {
+        $key      = 'test.txt';
+        $filePath = './README.md';
+        $result   = $this->s3->put($key, $filePath);
+        $this->assertTrue($result);
+    }
+
+    public function testGet()
+    {
+        $key      = 'test.txt';
+        $filePath = './test.txt';
+        $result   = $this->s3->get($key, $filePath);
+        $this->assertTrue($result);
+        $this->assertFileExists($filePath);
+        unlink($filePath);
+    }
+
+    public function testDelete()
+    {
+        $key    = 'test.txt';
+        $result = $this->s3->delete($key);
+        $this->assertTrue($result);
+    }
+
+    public function testExist()
+    {
+        $key    = 'test.txt';
+        $result = $this->s3->exist($key);
+        $this->assertFalse($result);
+    }
+
+    public function testUrl()
+    {
+        $key    = 'test.txt';
+        $expire = 3600;
+        $url    = $this->s3->url($key, $expire);
+        $this->assertStringContainsString($key, $url);
+    }
+
+    public function testStaticUrl()
+    {
+        $key = 'test.txt';
+        $url = $this->s3->url($key, -1);
+        var_dump($url);
+        $this->assertStringEndsWith($key, $url);
+
+    }
+
+    public function testPutUrl()
+    {
+        $key         = 'test.txt';
+        $contentType = 'text/plain';
+        $expire      = 3600;
+
+        $result = $this->s3->putUrl($key, $contentType, $expire);
+
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result[0]);
+        $this->assertNotEmpty($result[1]);
+    }
+}
